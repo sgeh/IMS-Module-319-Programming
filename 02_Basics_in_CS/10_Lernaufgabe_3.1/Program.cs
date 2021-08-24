920d552dcfe2bb9629deb0455d5e15768f0c6fb1
Silvan Gehrig <silvan.gehrig@ost.ch>
2021-08-24 19:35:09 +0200

    Add solutions for day 2.

---
+++ 02_Basics_in_CS/10_Lernaufgabe_3.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bwz.Rappi
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\t   /--------------------\\");
+            Console.WriteLine("\t  /--    HALLO WELT    --\\");
+            Console.WriteLine("\t /------------------------\\");
+        }
+    }
+}
---
+++ 02_Basics_in_CS/10_Lernaufgabe_3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bwz.Rappi
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("[ a * b = c ]");
+
+            Console.WriteLine("Bitte geben Sie a ein:");
+            string aInput = Console.ReadLine();
+            int a = Convert.ToInt32(aInput);
+
+            Console.WriteLine("Bitte geben Sie b ein:");
+            string bInput = Console.ReadLine();
+            int b = Convert.ToInt32(bInput);
+
+            int c = a * b;
+            Console.WriteLine($"[ {a} * {b} = {c} ]");
+        }
+    }
+}
