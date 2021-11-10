b39002a181ffa1baed9af0d836eaff491943ba09
Silvan Gehrig <silvan.gehrig@ost.ch>
2021-11-10 22:22:40 +0100

    Add additional task solutions for day 6.

---
+++ 06_Debugging/04_Debugging_1.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bwz.Rappi
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine($"Alle Grossbuchstaben:");
+
+			for (int i = 0; i < 26; i = i + 1)
+            {
+				for (int j = 0; j < i; j = j + 1)
+				{
+					char current = Convert.ToChar(j + 65);
+					Console.Write($"{current} ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
